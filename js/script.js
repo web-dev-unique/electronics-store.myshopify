34c459cc009e50f5b397b21f553c9270db8b489e
web-dev-unique <rajpatil88052@gmail.com>
2023-12-11 12:56:08 +0530

    live projects

--- js/script.js
+++ js/script.js
@@ -6,11 +6,6 @@
 //  ========= loader page start ===============
 
 
-function move() {
-    document.getElementById("preload").style.display = "none";
-}
-
-
 gsap.to(".box", {
     opacity: 1,
     duration: 0.5,
@@ -19,6 +14,13 @@ gsap.to(".box", {
     stagger: 0.4,
     scale: .0,
 })
+
+
+function move() {
+    document.getElementById("preload").style.display = "none";
+}
+
+
 
 
 //  ========= loader page end ===============
@@ -112,3 +114,27 @@ var swiper = new Swiper(".mySwiper", {
   /* ================= section4 =============== */
   /* ================= section4 =============== */ 
 
+
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
